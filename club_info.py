a8f438f25a92c7605e9195e398d3ec25333d3b3a
SA-9R-Lajom <lajom.jat@obmontessori.edu.ph>
2025-12-10 16:53:18 +0800

    Add files via upload

---
+++ club_info.py
@@ -0,0 +1,16 @@
+club = {
+    "name": "Buisness/Entrepreneurship Club",
+    "description": "A club for students interested in business and entrepreneurship, providing opportunities to learn about starting and managing businesses.",
+    "meeting_times": "Every Friday, 2:30PM - 5:30 PM",
+    "location": "Room 211",
+    "moderator": "Ms. Onefre",
+    "members": 42
+
+}
+def display_club_info(club):
+    print(f"Club Name: {club['name']}")
+    print(f"Description: {club['description']}")
+    print(f"Meeting Times: {club['meeting_times']}")
+    print(f"Location: {club['location']}")
+    print(f"Moderator: {club['moderator']}")
+    print(f"Number of Members: {club['members']}")
